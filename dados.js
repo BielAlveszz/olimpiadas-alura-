92211fa2dc17932092c932b6ce6547c87037782a
Gabriel Gonçalves Alves <167901465+BielAlveszz@users.noreply.github.com>
2024-09-06 17:48:55 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela se tornou a mais jovem medalhista olímpica da história do Brasil.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags : "skate fada sk8"
+    },
+
+    {
+        titulo : "Rebeca Andrade",
+        descricao : "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros e se tornou um dos maiores nomes do esporte nacional. Com sua força, determinação e talento, ela coleciona medalhas em competições nacionais e internacionais, incluindo os Jogos Olímpicos",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags : "ginastica ginástica ouro"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao : "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nas Olimpíadas de Paris. Sua conquista histórica colocou o Brasil no topo do pódio do judô feminino.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags : "judo judô judoca judoka"
+    }
+];
+
